b24594b681316efe6e14e4f50bd85b54686da12c
thanhnhan311201 <phannguyenthanhnhan3112@gmail.com>
2022-04-25 09:19:37 +0700

    update feature export to sheet

---
+++ register/static/register/javascript/registration.js
@@ -0,0 +1,31 @@
+$("#registration_form").submit(async (event) => {
+    var data = {};
+    var informations = {};
+    var school = {1: 'Trường Đại học Công nghệ Thông tin - UIT',
+                    2: 'Trường Đại học Khoa học Xã hội và Nhân văn - HCMUSSH',
+                    3: 'Trường Đại học Khoa học Tự nhiên - HCMUS',
+                    4: 'Trường Đại học Bách khoa - HCMUT',
+                    5: 'Trường Đại học Quốc tế - HCMIU'
+                };  
+
+    let registration_form =  document.getElementById('registration_form');
+    let formData = new FormData(registration_form);
+    
+    formData.forEach((value, key) => {
+        if (key.includes('school')) {
+            informations[key] = school[value];
+        } else {
+            informations[key] = value;
+        }
+    });
+    data["informations"] = informations;
+
+    fetch('https://us-central1-ucpc-348205.cloudfunctions.net/export_2_sheet', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        })
+        .then(res => console.log(res))
+});
