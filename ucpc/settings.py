d6f6f9586948d414a0ae0a9fdbcdbdcf1d03f694
thanhnhan311201 <phannguyenthanhnhan3112@gmail.com>
2022-05-02 21:18:12 +0700

    update

--- ucpc/settings.py
+++ ucpc/settings.py
@@ -24,15 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY','changeme')
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'django-insecure-_v9a$!$g(&$$h3b@fq*mp&pp7rcscw9^y+626%o60e1gwetx76'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(int(os.environ.get('DEBUG',1)))
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'ucpc-web.herokuapp.com']
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
 
 if ALLOWED_HOSTS_ENV:
